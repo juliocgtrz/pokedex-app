ad7ae9f16d4d058ab650f5c6046433ae7413cfbf
Julio G <juliocgtrz@gmail.com>
2023-06-14 22:54:22 -0600

    Update scripts.js
    removed a spacing for the comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokemonList = [ // data for pokedex
+let pokemonList = [ //data for pokedex
     {name: 'Bulbasaur', number: 1, height: 7, type: ['grass', 'poison']}
     {name: 'Charmander', number: 4, height: 6, type: 'fire'}
     {name: 'Squirtle', number: 7, height: 5, type: 'water'}
