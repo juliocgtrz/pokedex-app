21fef3cccf4eb0411541b20ae88911d27683eb01
Julio G <juliocgtrz@gmail.com>
2023-06-14 22:48:45 -0600

    Update scripts.js
    Began compiling data for pokedex

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let pokemonList = [ // data for pokedex
+    {name: 'Bulbasaur', number: 1, height: 7, type: ['grass', 'poison']}
+    {name: 'Charmander', number: 4, height: 6, type: 'fire'}
+    {name: 'Squirtle', number: 7, height: 5, type: 'water'}
+    {name: 'Caterpie', number: 10, height: 3, type: 'bug'}
+    {name: 'Weedle', number: 13, height: 3, type: ['bug', 'poison']}
+    {name: 'Pidgey', number: 16, height: 3, type: ['flying', 'normal']}
+];
