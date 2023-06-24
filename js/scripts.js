72124160a26d47a5a54d2eb10fa79d9821dea407
Julio G <juliocgtrz@gmail.com>
2023-06-23 21:50:43 -0600

    Update scripts.js
    Updated the height of each pokemon to more accurately reflect the value in meters

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 let pokemonList = [ //data for pokedex
-    {name: 'Bulbasaur', number: 1, height: 7, type: ['grass', 'poison']},
-    {name: 'Charmander', number: 4, height: 6, type: 'fire'},
-    {name: 'Squirtle', number: 7, height: 5, type: 'water'},
-    {name: 'Caterpie', number: 10, height: 3, type: 'bug'},
-    {name: 'Weedle', number: 13, height: 3, type: ['bug', 'poison']},
-    {name: 'Pidgey', number: 16, height: 3, type: ['flying', 'normal']},
-    {name: 'Rattata', number: 19, height: 3, type: 'normal'},
-    {name: 'Spearow', number: 21, height: 3, type: ['flying', 'normal']},
+    {name: 'Bulbasaur', number: 1, height: 0.7, type: ['grass', 'poison']},
+    {name: 'Charmander', number: 4, height: 0.6, type: 'fire'},
+    {name: 'Squirtle', number: 7, height: 0.5, type: 'water'},
+    {name: 'Caterpie', number: 10, height: 0.3, type: 'bug'},
+    {name: 'Weedle', number: 13, height: 0.3, type: ['bug', 'poison']},
+    {name: 'Pidgey', number: 16, height: 0.3, type: ['flying', 'normal']},
+    {name: 'Rattata', number: 19, height: 0.3, type: 'normal'},
+    {name: 'Spearow', number: 21, height: 0.3, type: ['flying', 'normal']},
     {name: 'Ekans', number: 23, height: 2, type: 'poison'},
-    {name: 'Pikachu', number: 25, height: 4, type: 'electric'}
+    {name: 'Pikachu', number: 25, height: 0.4, type: 'electric'}
 ];
 
 //this will write out each pokemon's name on the webpage with a space in between
