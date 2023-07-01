e662eff1969bb7c5300658cac5342ac169772cdd
Julio G <juliocgtrz@gmail.com>
2023-07-01 00:20:30 -0600

    Update scripts.js
    moved pokemonList to wrap it in a IIFE, removed the previous loop function, refactored the code to use a forEach loop function that iterates over each pokemon in the repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,34 @@
-let pokemonList = [ //data for pokedex
-    {name: 'Bulbasaur', number: 1, height: 0.7, type: ['grass', 'poison']},
-    {name: 'Charmander', number: 4, height: 0.6, type: 'fire'},
-    {name: 'Squirtle', number: 7, height: 0.5, type: 'water'},
-    {name: 'Caterpie', number: 10, height: 0.3, type: 'bug'},
-    {name: 'Weedle', number: 13, height: 0.3, type: ['bug', 'poison']},
-    {name: 'Pidgey', number: 16, height: 0.3, type: ['flying', 'normal']},
-    {name: 'Rattata', number: 19, height: 0.3, type: 'normal'},
-    {name: 'Spearow', number: 21, height: 0.3, type: ['flying', 'normal']},
-    {name: 'Ekans', number: 23, height: 2, type: 'poison'},
-    {name: 'Pikachu', number: 25, height: 0.4, type: 'electric'}
-];
-
-//this will write out each pokemon's name on the webpage with a space in between and a message next to the pokemon fitting the conditional
-for (let i = 0; i < pokemonList.length; i++){
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
-    if (pokemonList[i].height > 1) {
-        document.write(' - Wow, that\'s big!');
+// wrapping the pokemonList in an Immediately Invoked Function Expression (IIFE)
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name: 'Bulbasaur', number: 1, height: 0.7, type: ['grass', 'poison']},
+        {name: 'Charmander', number: 4, height: 0.6, type: 'fire'},
+        {name: 'Squirtle', number: 7, height: 0.5, type: 'water'},
+        {name: 'Caterpie', number: 10, height: 0.3, type: 'bug'},
+        {name: 'Weedle', number: 13, height: 0.3, type: ['bug', 'poison']},
+        {name: 'Pidgey', number: 16, height: 0.3, type: ['flying', 'normal']},
+        {name: 'Rattata', number: 19, height: 0.3, type: 'normal'},
+        {name: 'Spearow', number: 21, height: 0.3, type: ['flying', 'normal']},
+        {name: 'Ekans', number: 23, height: 2, type: 'poison'},
+        {name: 'Pikachu', number: 25, height: 0.4, type: 'electric'}
+    ];
+  
+    function getAll() {
+      return pokemonList;
     }
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+  // this code writes the name of each pokemon in a list with a break in between each pokemon name
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write(pokemon.name);
     document.write('<br/>');
-}
+  });
+  
